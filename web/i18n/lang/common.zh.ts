0e0a4c02d576d35b964a603ce4465101325d4c04
zhanghan <zhanghan@maiziit.com>
2023-11-22 10:32:05 +0800

    test: test is work

--- web/i18n/lang/common.zh.ts
+++ web/i18n/lang/common.zh.ts
@@ -82,7 +82,7 @@ const translation = {
   },
   menus: {
     status: 'beta',
-    explore: '探索',
+    explore: '探索发现',
     apps: '构建应用',
     plugins: '插件',
     pluginsTips: '集成第三方插件或创建与 ChatGPT 兼容的 AI 插件。',
